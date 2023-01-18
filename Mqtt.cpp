b5a599d3da3885a14c514bcc06052e54f358f4a9
Andrea Carrara <andrea.carrara@libero.it>
2023-01-18 11:09:52 +0100

    Publish mods

--- Mqtt.cpp
+++ Mqtt.cpp
@@ -15,11 +15,18 @@ void Mqtt::Stop()
 
 int Mqtt::Publish(const char *topic,const char *msg) {
     //if((xEventGroupGetBits(s_wifi_event_group) & MQTT_CONNECTED_BIT) == false) return;
-    ESP_LOGI(TAG, "Mqtt::Publish topic=%s, msg=%s", topic, msg);
-    if(!client) return -1;
-    if(!topic) return -2;
-    if(!msg) return -3;
-    return esp_mqtt_client_publish(client, topic, msg, strlen(msg), 0, 0);
+    int j=0;
+    if(!client) j= -1;
+    if(!topic) j= -2;
+    if(!msg) j= -3;
+    if (j<0) {
+        ESP_LOGW(TAG, "Mqtt::Publish error=%d", j);
+    } else {
+        j= esp_mqtt_client_publish(client, topic, msg, strlen(msg), 0, 0);
+    }
+    if(j!=-1) ESP_LOGI(TAG, "Mqtt::Publish topic=%s, msg=%s", topic, msg);
+    return j;
+
 }
 
 int Mqtt::Subscribe(const char *topic)
@@ -54,17 +61,23 @@ void Mqtt::Init(const char* _username, const char* _password,const char* _uri,co
     }
     esp_mqtt_client_config_t mqtt_cfg = {};
     mqtt_cfg.broker.address.uri=_uri;
+    mqtt_cfg.broker.verification.certificate=_cert;
+    //mqtt_cfg.broker.verification.certificate_len=strlen(_cert)+1;
+    mqtt_cfg.broker.verification.skip_cert_common_name_check=true;
+
     mqtt_cfg.credentials.username=_username;
     mqtt_cfg.credentials.authentication.password=_password;
-    mqtt_cfg.credentials.authentication.certificate=_cert;
-    mqtt_cfg.broker.verification.skip_cert_common_name_check=true;
+
     mqtt_cfg.network.disable_auto_reconnect=false;
+    ESP_LOGD(TAG,"cert: %s",_cert);
+    
     client = esp_mqtt_client_init(&mqtt_cfg);
     if(client==NULL) {
         ESP_LOGE(TAG,"esp_mqtt_client_init fails");
         return;
     }
     esp_mqtt_client_register_event(client, (esp_mqtt_event_id_t)ESP_EVENT_ANY_ID, &Mqtt::mqtt_event_handler, (void*)this);
+    esp_mqtt_client_start(client);
 }
 
 
